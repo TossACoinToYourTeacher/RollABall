02125f7d7160db354e57b107d69b4c48923ab6ce
TossACoinToYourTeacher <59828461+TossACoinToYourTeacher@users.noreply.github.com>
2021-04-27 00:35:12 +0200

    Lab 1_ Roll A Ball

---
+++ RollABall/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,73 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.InputSystem;
+using TMPro;
+
+public class PlayerController : MonoBehaviour
+{
+    //Public variables
+    public float speed = 0;
+    public TextMeshProUGUI countText;
+    public GameObject winTextObject;
+    public AudioSource coin_sound;
+    public AudioSource win_sound;
+
+    //Private variables
+    private AudioSource[] sounds;
+    private Rigidbody rb;
+    private int count;
+    private float movementX;
+    private float movementY;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+        count = 0;
+
+        SetCountText();
+        winTextObject.SetActive(false);
+
+        sounds = GetComponents<AudioSource>();
+        coin_sound = sounds[0];
+        win_sound = sounds[1];
+    }
+
+    void OnMove(InputValue movementValue)
+    {
+        Vector2 movementVector = movementValue.Get<Vector2>();
+
+        movementX = movementVector.x;
+        movementY = movementVector.y;
+    }
+    void SetCountText()
+    {
+        countText.text = "Count : " + count.ToString();
+        if(count >= 12)
+        {
+            winTextObject.SetActive(true);
+            win_sound.Play();
+        }
+    }
+
+    void FixedUpdate()
+    {
+        Vector3 movement = new Vector3(movementX, 0.0f, movementY);
+
+        rb.AddForce(movement * speed);
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.CompareTag("PickUp"))
+        {
+            other.gameObject.SetActive(false);
+            count += 1;
+
+            coin_sound.Play();
+
+            SetCountText();
+        }
+    }
+}
